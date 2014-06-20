f6ad6c78c0618f37eb3bc1a6fae5467d92160f0e
elricL <Sriram137@gmail.com>
2014-06-21 01:08:20 +0530

    Change forward logic

--- example.py
+++ example.py
@@ -16,24 +16,26 @@ def response_sip_route():
         from_number = request.args.get('CLID', None)
         if from_number is None:
             from_number = request.args.get('From', '')
-        caller_name = request.args.get('CallerName', '')
+        # caller_name = request.args.get('CallerName', '')
     elif request.method == 'POST':
         to_number = request.form.get('To', None)
         from_number = request.form.get('CLID', None)
         if from_number is None:
             from_number = request.form.get('From', '')
-        caller_name = request.form.get('CallerName', '')
+        # caller_name = request.form.get('CallerName', '')
     else:
         return make_response('Method not allowed.')
 
     response = plivo.Response()
+    to_number = "sip:elricl140620163139@phone.plivo.com"
     if not to_number:
         response.addHangup()
     else:
-        if to_number[:4] == 'sip:':
-            response.addDial(callerName=caller_name).addUser(to_number)
-        else:
-            response.addDial(callerId=from_number).addNumber(to_number)
+        response.addDial().addUser(to_number)
+        # if to_number[:4] == 'sip:':
+            # response.addDial().addUser(to_number)
+        # else:
+            # response.addDial().addNumber(to_number)
 
     response = make_response(response.to_xml())
     response.headers['Content-Type'] = 'text/xml'
